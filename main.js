0dbdaf74a0ac7f222c8b20c4e3c4bffba2d31114
lhuang23 <lhuang@mail.dyu.edu.tw>
2013-10-19 15:45:42 +0800

    10 balls with random colors, fix #8

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var gameModule=(function (){
 	var timeoutVar,
-		counter=0;
-	function start(){
-		var canvas = document.getElementById('game'),
-			ctx = canvas.getContext('2d'),
-			ballX=Math.floor(Math.random()*300),
-			ballY=Math.floor(Math.random()*450),
-			ballR=Math.floor(Math.random()*100);
+		counter=0,
+		canvas = document.getElementById('game'),
+		ctx = canvas.getContext('2d');
 
 		canvas.width = 480;
 		canvas.height = 320;
 
-		ctx.fillStyle = 'black';
+	function start(){
+		var ballX=Math.floor(Math.random()*300),
+			ballY=Math.floor(Math.random()*450),
+			ballR=Math.floor(Math.random()*50);
+			color=Math.floor(Math.random()*1000);
+
+		ctx.fillStyle = '#'+color.toString();
 		ctx.beginPath();
 		ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
 		ctx.fill();
