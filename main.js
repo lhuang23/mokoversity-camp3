9fd016fe6b4572da88a9de5f510a3c33086a1f72
lhuang23 <lhuang@mail.dyu.edu.tw>
2013-10-19 15:09:51 +0800

    fix #7

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-(function (){
-	var canvas = document.getElementById('game'),
-		ctx = canvas.getContext('2d'),
-		ballX=Math.floor(Math.random()*300),
-		ballY=Math.floor(Math.random()*500),
-		ballR=Math.floor(Math.random()*100);
+var gameModule=(function (){
+	function start(){
+		var canvas = document.getElementById('game'),
+			ctx = canvas.getContext('2d'),
+			ballX=Math.floor(Math.random()*300),
+			ballY=Math.floor(Math.random()*450),
+			ballR=Math.floor(Math.random()*100);
 
-	canvas.width = 480;
-	canvas.height = 320;
+		canvas.width = 480;
+		canvas.height = 320;
 
-	ctx.fillStyle = 'black';
-	ctx.beginPath();
-	ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
-	ctx.fill();
+		ctx.fillStyle = 'black';
+		ctx.beginPath();
+		ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
+		ctx.fill();
+	}
+	return {
+		start: start
+	}
 })();
+
+gameModule.start();
