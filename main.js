087fa5e8789855d0770bb1244bba1a1e8b9b8814
lhuang23 <lhuang@mail.dyu.edu.tw>
2013-10-20 10:14:54 +0800

    fix #10

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 var gameModule=(function (){
 	var timeoutVar,
 		counter=0,
+		colors = ['#ff0000', '#0000ff', 'yellow'],
+        length = colors.length,
+
 		canvas = document.getElementById('game'),
 		ctx = canvas.getContext('2d');
 
-		canvas.width = 640;
-		canvas.height = 480;
+	canvas.width = 640;
+	canvas.height = 480;
 
 	function start(){
 		var ballX=Math.floor(Math.random()*440)+100,
 			ballY=Math.floor(Math.random()*280)+100,
 			ballR=Math.floor(Math.random()*90)+10;
-			color1=Math.floor(Math.random()*256);
-			color2=Math.floor(Math.random()*256);
-			color3=Math.floor(Math.random()*256);
+		//	color1=Math.floor(Math.random()*256);    //red color
+		//	color2=Math.floor(Math.random()*256);    //green color
+		//	color3=Math.floor(Math.random()*256);    //blue color
 
-		ctx.fillStyle = '#'+color1.toString(16)+color2.toString(16)+color3.toString(16);
+		//ctx.fillStyle = '#'+color1.toString(16)+color2.toString(16)+color3.toString(16);   
+		ctx.fillStyle = colors[counter % length];
 		ctx.beginPath();
 		ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
 		ctx.fill();
